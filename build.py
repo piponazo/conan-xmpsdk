d1ce9bbc05079fe6172a8d21365c51308d238d0c
Luis Diaz Mas <piponazo@gmail.com>
2018-05-16 12:18:07 +0200

    Only build in release mode

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=['x86_64'])
+    builder = ConanMultiPackager(archs=['x86_64'], build_types=['Release'])
     builder.add_common_builds(shared_option_name="XmpSdk:shared")
     builder.run()
