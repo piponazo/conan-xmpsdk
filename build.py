21a7907bde970734b24f1debb59f0ecea2307987
Luis Díaz Más <piponazo@gmail.com>
2018-05-17 17:19:55 +0200

    Do not consider MT runtime. It is not managed by XmpSdk

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(archs=['x86_64'], build_types=['Release'])
+    builder = ConanMultiPackager(archs=['x86_64'], build_types=['Release'],
+                                visual_runtimes=['MD'])
     builder.add_common_builds(shared_option_name="XmpSdk:shared")
     builder.run()
